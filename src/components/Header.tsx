3392c92c5392e798e53a6eb5440d0ea3c6151335
dealbisac <chand.dipendra19@gmail.com>
2026-02-10 20:41:59 +0100

    feat: add header component with authentication buttons for user sign-in

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center p-4 border-b">
+      <h1 className="text-xl font-bold">Email Sender</h1>
+      
+      <div>
+        {/* Show this if the user is NOT logged in */}
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="bg-black text-white px-4 py-2 rounded text-sm">
+              Sign In
+            </button>
+          </SignInButton>
+        </SignedOut>
+
+        {/* Show this if the user IS logged in */}
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </div>
+    </header>
+  );
+}
